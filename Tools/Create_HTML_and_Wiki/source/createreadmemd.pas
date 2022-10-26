108fc484b1b2450fee3e0744e480281ce82a89c8
sechshelme <m_burkhard@gmx.ch>
2022-10-26 13:19:53 +0200

    readme

--- Tools/Create_HTML_and_Wiki/source/createreadmemd.pas
+++ Tools/Create_HTML_and_Wiki/source/createreadmemd.pas
@@ -509,7 +509,8 @@ begin
     end else if Pos('//image ', slUnit[i]) > 0 then begin
       s:=Copy(slUnit[i], 9);
 //      slHTML.Add('<img src="' + Copy(slUnit[i], 9) + '" alt="Selfhtml"><br><br>');
-      slHTML.Add('!['+s+']('+s+')'+LineEnding);
+slHTML.Add('!['+s+']('+s+')');
+slHTML.Add('');
     end else if Pos('//lineal', slUnit[i]) > 0 then begin
 //      slHTML.Add('<hr><br>');
             slHTML.Add('---');
