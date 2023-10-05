7babd8d6ba1ea2ea0a4619fefe57ce0628c22d40
sechshelme <you@example.com>
2023-10-05 16:29:17 +0200

    pixelbuffer

--- Versuche/00_-_Textur_PBO (Kopie)/unit1.pas
+++ Versuche/00_-_Textur_PBO (Kopie)/unit1.pas
@@ -10,12 +10,7 @@ uses
   dglOpenGL, oglDebug,
   oglContext, oglShader, oglVector, oglMatrix;
 
-const
-  TexturSize = 8;
-
 type
-  TTexture32 = packed array[0..TexturSize - 1, 0..TexturSize - 1, 0..3] of byte;
-  PTexture32 = ^TTexture32;
 
   { TForm1 }
 
@@ -29,7 +24,6 @@ type
     Shader: TShader; // Shader Klasse
     procedure CreateScene;
     procedure ogcDrawScene(Sender: TObject);
-    function GenTexture: TTexture32;
   public
   end;
 
@@ -53,8 +47,15 @@ const
     (0.0, 0.0), (1.0, 0.0), (1.0, 1.0));
 
 type
+  TTexture32 = packed array[0..1, 0..1, 0..3] of byte;
+  PTexture32 = ^TTexture32;
+
+var
+  Textur32_0: TTexture32 = ((($FF, $00, $00, $FF), ($00, $FF, $00, $FF)), (($00, $00, $FF, $FF), ($FF, $00, $00, $FF)));
+
+type
   TVB = record
-    PBO,
+    PBO: array[0..1] of GLuint;
     VAO,
     VBOVertex,        // Vertex-Koordinaten
     VBOTex: GLuint;   // Textur-Koordianten
@@ -62,6 +63,9 @@ type
 
 var
   VBQuad: TVB;
+  //code-
+  RotMatrix, ScaleMatrix, ProdMatrix: TMatrix;
+  Matrix_ID: GLint;
 
 var
   textureID: GLuint;
@@ -80,31 +84,44 @@ begin
   InitOpenGLDebug;
 end;
 
-function TForm1.GenTexture: TTexture32;
-var
-  x, y, i: integer;
-begin
-  for x := 0 to TexturSize - 1 do begin
-    for y := 0 to TexturSize - 1 do begin
-      for i := 0 to 2 do begin
-        Result[x, y, i] := Random($FF);
-      end;
-    end;
-  end;
-end;
-
+//code+
 procedure TForm1.CreateScene;
 begin
-  Shader := TShader.Create([
-    GL_VERTEX_SHADER, FileToStr('Vertexshader.glsl'),
-    GL_FRAGMENT_SHADER, FileToStr('Fragmentshader.glsl')]);
+  Shader := TShader.Create([FileToStr('Vertexshader.glsl'), FileToStr('Fragmentshader.glsl')]);
   with Shader do begin
     UseProgram;
+    Matrix_ID := UniformLocation('mat');
     glUniform1i(UniformLocation('Sampler'), 0);  // Dem Sampler 0 zuweisen.
   end;
-  glClearColor(0.6, 0.6, 0.4, 1.0); // Hintergrundfarbe
+  //code-
+  RotMatrix.Identity;
+  ScaleMatrix.Identity;
+  ScaleMatrix.Scale(0.7);
+  ProdMatrix.Identity;
 
   glGenVertexArrays(1, @VBQuad.VAO);
+
+  // https://www.songho.ca/opengl/gl_pbo.html
+
+  glGenTextures(1, @textureID);                 // Erzeugen des Textur-Puffer.
+  glBindTexture(GL_TEXTURE_2D, textureID);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 2, 2, 0, GL_RGBA, GL_UNSIGNED_BYTE, nil);
+  glBindTexture(GL_TEXTURE_2D, 0);
+
+
+  glGenBuffers(2, VBQuad.PBO);
+  glBindBuffer(GL_PIXEL_UNPACK_BUFFER, VBQuad.PBO[0]);
+  glBufferData(GL_PIXEL_UNPACK_BUFFER, SizeOf(Textur32_0), nil, GL_STREAM_DRAW);
+  glBindBuffer(GL_PIXEL_UNPACK_BUFFER, VBQuad.PBO[1]);
+  glBufferData(GL_PIXEL_UNPACK_BUFFER, SizeOf(Textur32_0), nil, GL_STREAM_DRAW);
+  glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+
+
+  glClearColor(0.6, 0.6, 0.4, 1.0); // Hintergrundfarbe
+
   glGenBuffers(1, @VBQuad.VBOVertex);
   glGenBuffers(1, @VBQuad.VBOTex);
 
@@ -118,42 +135,50 @@ begin
   glBufferData(GL_ARRAY_BUFFER, sizeof(TextureVertex), @TextureVertex, GL_STATIC_DRAW);
   glEnableVertexAttribArray(10);
   glVertexAttribPointer(10, 2, GL_FLOAT, False, 0, nil);
-
-
-  // https://www.songho.ca/opengl/gl_pbo.html
-
-  glGenBuffers(1, @VBQuad.PBO);
-  glBindBuffer(GL_PIXEL_UNPACK_BUFFER, VBQuad.PBO);
-  glBufferData(GL_PIXEL_UNPACK_BUFFER, TexturSize * TexturSize * 4, nil, GL_DYNAMIC_DRAW);
-  glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
-
-  glGenTextures(1, @textureID);                 // Erzeugen des Textur-Puffer.
-  glBindTexture(GL_TEXTURE_2D, textureID);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, TexturSize, TexturSize, 0, GL_RGBA, GL_UNSIGNED_BYTE, nil);
-  glBindTexture(GL_TEXTURE_2D, 0);
 end;
 
 procedure TForm1.ogcDrawScene(Sender: TObject);
+const
+  index: integer = 0;
 var
-  buf: TTexture32;
+  nextIndex: integer;
+var
+  mapBuffer: PTexture32;
 begin
   glClear(GL_COLOR_BUFFER_BIT);
+
   Shader.UseProgram;
 
-  glBindTexture(GL_TEXTURE_2D, textureID);
-  glBindBuffer(GL_PIXEL_UNPACK_BUFFER, VBQuad.PBO);
-  glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, TexturSize, TexturSize, GL_RGBA, GL_UNSIGNED_BYTE, nil);
-  buf := GenTexture;
-  glBufferSubData(GL_PIXEL_UNPACK_BUFFER, 0, TexturSize * TexturSize * 4, @buf);
+  index := (index + 1) mod 2;
+  nextIndex := (index + 1) mod 2;
 
+  glBindTexture(GL_TEXTURE_2D, textureID);
+  glBindBuffer(GL_PIXEL_UNPACK_BUFFER, VBQuad.PBO[index]);
+  glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 2, 2, GL_RGBA, GL_UNSIGNED_BYTE, nil);
+  glBindBuffer(GL_PIXEL_UNPACK_BUFFER, VBQuad.PBO[nextIndex]);
+  glBufferData(GL_PIXEL_UNPACK_BUFFER, SizeOf(Textur32_0), nil, GL_STREAM_DRAW);
+
+  mapBuffer := glMapBuffer(GL_PIXEL_UNPACK_BUFFER, GL_WRITE_ONLY);
+
+  if mapBuffer <> nil then begin
+ //   mapBuffer^ := Textur32_0;
+    mapBuffer^[0,0,0]:=Random($FF);
+    mapBuffer^[0,1,0]:=Random($FF);
+    mapBuffer^[1,0,0]:=Random($FF);
+    mapBuffer^[1,1,0]:=Random($FF);
+    glUnmapBuffer(GL_PIXEL_UNPACK_BUFFER);
+  end;
+  glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+
+
+
+
+  ProdMatrix := ScaleMatrix * RotMatrix;
+  ProdMatrix.Uniform(Matrix_ID);
 
   // Zeichne Quadrat
   glBindVertexArray(VBQuad.VAO);
   glDrawArrays(GL_TRIANGLES, 0, Length(QuadVertex));
-
-  glBindTexture(GL_TEXTURE_2D, 0);
 
   ogc.SwapBuffers;
 end;
@@ -162,17 +187,20 @@ procedure TForm1.FormDestroy(Sender: TObject);
 begin
   Timer1.Enabled := False;
 
-  glDeleteTextures(1, @textureID);
+  glDeleteTextures(1, @textureID);       // Textur-Puffer frei geben.
   glDeleteVertexArrays(1, @VBQuad.VAO);
   glDeleteBuffers(1, @VBQuad.VBOVertex);
   glDeleteBuffers(1, @VBQuad.VBOTex);
-  glDeleteBuffers(1, @VBQuad.PBO);
+  glDeleteBuffers(2, VBQuad.PBO);
 
   Shader.Free;
 end;
 
 procedure TForm1.Timer1Timer(Sender: TObject);
+const
+  step: GLfloat = 0.01;
 begin
+  RotMatrix.RotateC(step);
   ogcDrawScene(Sender);
 end;
 
