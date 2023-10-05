6e89fea185b5f897764d8ac5dcc8a08813e2015e
sechshelme <you@example.com>
2023-10-05 19:34:00 +0200

    pixelbuffer

--- Versuche/00_-_Textur_PBO_Multitexture/Vertexshader.glsl
+++ Versuche/00_-_Textur_PBO_Multitexture/Vertexshader.glsl
@@ -3,12 +3,10 @@
 layout (location =  0) in vec3 inPos;   // Vertex-Koordinaten
 layout (location = 10) in vec2 inUV;    // Textur-Koordinaten
 
-uniform mat4 mat;
-
 out vec2 UV0;
 
 void main(void)
 {
-  gl_Position = mat * vec4(inPos, 1.0);
+  gl_Position = vec4(inPos, 1.0);
   UV0 = inUV;                           // Textur-Koordinaten weiterleiten.
 }
