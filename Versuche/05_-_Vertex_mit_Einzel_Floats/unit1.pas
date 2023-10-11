8c4bf3608b2db0d032893fdb282dff4de1fa883a
sechshelme <you@example.com>
2023-10-11 19:56:55 +0200

    feedback

--- Versuche/05_-_Vertex_mit_Einzel_Floats/unit1.pas
+++ Versuche/05_-_Vertex_mit_Einzel_Floats/unit1.pas
@@ -8,9 +8,9 @@ uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics,
   Dialogs, ExtCtrls,
   dglOpenGL,
-  oglContext, oglShader;
+  oglContext, oglShader, ogldebug;
 
-//image image.png
+  //image image.png
 (*
 Man kann die Vertex-Daten, auch alles in einen Daten-Block schreiben. Hier werden die Vector- und Color - Daten alle in einen Block geschrieben.
 In den vorherigen Beispielen hat es für die Vector- und  Color - Daten eine separate TFace-Array gehabt.
@@ -20,7 +20,7 @@ Variante2: <b>Vec0, ..., Vecn, Col0, ..., Coln</b>
 Die hat noch den Vorteil, das nur ein <b>VBO</b> angelegt werden muss, obwohl mehrere Attribute in der Array sind.
 *)
 
-//lineal
+  //lineal
 
 type
   TForm1 = class(TForm)
@@ -55,20 +55,23 @@ Variante 1: <b>XYZ XYZ XYZ XYZ XYZ XYZ RGB RGB RGB RGB RGB RGB</b>
 
 Bei dem zweiten Quadrat, sind die Y-Werte gespiegelt, es sollten zwei Quadrate sichtbar sein.
 *)
-//code+
+  //code+
 const
   QuadVektor0: array[0..1] of TFace2 =
     // Vec, Col, Vec, Col, ....
     (((-0.2, 0.6, 0.0, 1.0, 0.0, 0.0), (-0.2, 0.1, 0.0, 0.0, 1.0, 0.0), (0.2, 0.1, 0.0, 1.0, 1.0, 0.0)),
     ((-0.2, 0.6, 0.0, 1.0, 0.0, 0.0), (0.2, 0.1, 0.0, 1.0, 1.0, 0.0), (0.2, 0.6, 0.0, 0.0, 1.0, 1.0)));
-  QuadVektor1: array[0..3] of TFace =
+  QuadVektor1: array[0..35] of GLfloat = (
     // Vec
-    (((-0.2, -0.6, 0.0), (-0.2, -0.1, 0.0), (0.2, -0.1, 0.0)),
-    ((-0.2, -0.6, 0.0), (0.2, -0.1, 0.0), (0.2, -0.6, 0.0)),
+    -0.2, -0.2, 0.2, -0.2, 0.2, 0.2,
+    -0.6, -0.1, -0.1, -0.6, -0.1, -0.6,
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
     // Col
-    ((1.0, 0.0, 1.0), (1.0, 1.0, 0.0), (1.0, 1.0, 1.0)),
-    ((1.0, 0.0, 1.0), (1.0, 1.0, 1.0), (0.0, 1.0, 1.0)));
-//code-
+    1.0, 0.0, 1.0, 1.0, 1.0, 0.0,
+    0.0, 1.0, 1.0, 0.0, 1.0, 1.0,
+    1.0, 0.0, 1.0, 1.0, 1.0, 1.0
+    );
+  //code-
 
 type
   TVB = record
@@ -79,7 +82,7 @@ type
 var
   VBQuad0, VBQuad1: TVB;
 
-{ TForm1 }
+  { TForm1 }
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
@@ -92,6 +95,7 @@ begin
 
   CreateScene;
   InitScene;
+  //  ini
 end;
 
 procedure TForm1.CreateScene;
@@ -106,60 +110,60 @@ begin
   glGenBuffers(1, @VBQuad1.VBO);
 end;
 
-(*
-Hier die wichtigste Änderung:
-Relevant sind die zwei letzten Parameter von <b>glVertexAttribPointer(...</b>
-Was irritiert der einte Parameter ist direkt ein Integer, der andere braucht eine Typenumwandlung auf einen Pointer.
-Der zweitletzte Parameter (stride), gibt das <b>Byte</b> Offset, zum nächsten Attribut-Wert an, repektive die Schritt/Block-grösse.
-Der letzte Parameter (pointer), gibt die Position zum ersten Attribut-Wert an.
-Die Werte sind immer als <b>Byte</b>, somit muss man bei einem <b>glFloat</b> immer <b>4x</b> rechnen.
-
-Varinate 0:
-Die Vektoren beginnen bei 0, Die Grösse ist 24Byte = 6 glFloat x 4 entspricht <b>XYZRGB</b>.
-Die Farben beginnen beim 12Byte. Die Grösse ist mit 24Byte gleich wie bei den Vektoren.
-
-Varinate 1:
-Da die Vektoren hintereinander stehen, darf dieser Default (0) sein.
-Die Farben beginnen beim 72Byte.
-*)
 //code+
 procedure TForm1.InitScene;
 begin
-  glClearColor(0.6, 0.6, 0.4, 1.0); // Hintergrundfarbe
+  glClearColor(0.6, 0.6, 0.4, 1.0);
 
   // --- Daten für Quadrat 0
   glBindVertexArray(VBQuad0.VAO);
-  glBindBuffer(GL_ARRAY_BUFFER, VBQuad0.VBO); // Nur ein VBO erforderlich
+  glBindBuffer(GL_ARRAY_BUFFER, VBQuad0.VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(QuadVektor0), @QuadVektor0, GL_STATIC_DRAW);
 
-  // Vektor
   glEnableVertexAttribArray(10);
-  glVertexAttribPointer(10, 3, GL_FLOAT, False, 24, nil);  // nil = Pointer(0)
+  glVertexAttribPointer(10, 1, GL_FLOAT, False, 24, Pointer(0));
 
-  // Farbe
   glEnableVertexAttribArray(11);
-  glVertexAttribPointer(11, 3, GL_FLOAT, False, 24, Pointer(12));
+  glVertexAttribPointer(11, 1, GL_FLOAT, False, 24, Pointer(4));
+
+  glEnableVertexAttribArray(12);
+  glVertexAttribPointer(12, 1, GL_FLOAT, False, 24, Pointer(8));
+
+  glEnableVertexAttribArray(13);
+  glVertexAttribPointer(13, 1, GL_FLOAT, False, 24, Pointer(12));
+
+  glEnableVertexAttribArray(14);
+  glVertexAttribPointer(14, 1, GL_FLOAT, False, 24, Pointer(16));
+
+  glEnableVertexAttribArray(15);
+  glVertexAttribPointer(15, 1, GL_FLOAT, False, 24, Pointer(20));
 
   // --- Daten für Quadrat 1
   glBindVertexArray(VBQuad1.VAO);
-  glBindBuffer(GL_ARRAY_BUFFER, VBQuad1.VBO); // Nur ein VBO erforderlich
+  glBindBuffer(GL_ARRAY_BUFFER, VBQuad1.VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(QuadVektor1), @QuadVektor1, GL_STATIC_DRAW);
 
-  // Vektor
   glEnableVertexAttribArray(10);
-  glVertexAttribPointer(10, 3, GL_FLOAT, False, 12, nil);
+  glVertexAttribPointer(10, 1, GL_FLOAT, False, 4, Pointer(0));
 
-  // Farbe
   glEnableVertexAttribArray(11);
-  glVertexAttribPointer(11, 3, GL_FLOAT, False, 12, Pointer(72));
+  glVertexAttribPointer(11, 1, GL_FLOAT, False, 4, Pointer(24));
+
+  glEnableVertexAttribArray(12);
+  glVertexAttribPointer(12, 1, GL_FLOAT, False, 4, Pointer(48));
+
+  glEnableVertexAttribArray(13);
+  glVertexAttribPointer(13, 1, GL_FLOAT, False, 4, Pointer(72));
+
+  glEnableVertexAttribArray(14);
+  glVertexAttribPointer(14, 1, GL_FLOAT, False, 4, Pointer(96));
+
+  glEnableVertexAttribArray(15);
+  glVertexAttribPointer(15, 1, GL_FLOAT, False, 4, Pointer(120));
 end;
+
 //code-
 
-(*
-Das Zeichnen ist gleich, wie wen man separate Datenblöcke hätte. 
-Es wurde das <b>Length(...</b> entfernt, da die einte Array zwei und die andere vier Elemente hat.
-Was aber sicher ist, das beide Quadrate aus sechs Vektoren bestehen.
-*)
 procedure TForm1.ogcDrawScene(Sender: TObject);
 begin
   glClear(GL_COLOR_BUFFER_BIT);
@@ -206,5 +210,3 @@ Im Shader gibt es keine Änderung, da es diesem egal ist, wie <b>glVertexAttribP
 //includeglsl Fragmentshader.glsl
 
 end.
-
-
