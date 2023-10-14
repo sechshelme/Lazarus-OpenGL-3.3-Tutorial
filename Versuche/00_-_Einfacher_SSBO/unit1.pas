dd3de118f3e9fd9b06ccb65ec6005b7fc110b0db
sechshelme <you@example.com>
2023-10-14 12:14:55 +0200

    ssbo

--- Versuche/00_-_Einfacher_SSBO/unit1.pas
+++ Versuche/00_-_Einfacher_SSBO/unit1.pas
@@ -9,26 +9,10 @@ uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics,
   Dialogs, ExtCtrls, ComCtrls, StdCtrls, Menus,
   dglOpenGL,
-  oglContext, oglShader, oglVector, oglMatrix;
+  oglContext, oglShader, oglVector, oglMatrix, oglDebug;
 
   //image image.png
-(*
-Bis jetzt wurden alle Uniforms einzeln dem Shader übegeben.
-Wen man aber mehrer Werte übeergeben will, kann man die <b>Uniforms</b> zu einem <b>Block</b> zusammenfassen.
-Aus diesem Grund heisst dieser Puffer <b>Uniform</b> Buffer Object ( UBO ).
-
-Dies macht man mit einem Record. Dabei muss man auf eine <b>16Byte</b>-Ausrichtung achten.
-
-Die Material-Eigenschaften sind ein ideales Beispiel dafür.
-
-In diesem Beispiel sind die Kugeln aus Rubin.
-*)
-  //lineal
-
 type
-
-  { TForm1 }
-
   TForm1 = class(TForm)
     MainMenu1: TMainMenu;
     MenuItem1: TMenuItem;
@@ -43,9 +27,8 @@ type
     procedure Timer1Timer(Sender: TObject);
   private
     ogc: TContext;
-    Shader: TShader; // Shader Klasse
+    Shader: TShader;
     procedure CreateScene;
-    procedure InitScene;
     procedure ogcDrawScene(Sender: TObject);
     procedure ogcResize(Sender: TObject);
 
@@ -60,18 +43,8 @@ implementation
 
 {$R *.lfm}
 
-(*
-Hier wird der Record für die Material-Eigenschaften deklariert.
-
-Da ein <b>TVector3f</b> nur <b>12Byte</b> hat, muss man zum Aufrunden auf <b>16Byte</b> noch ein Padding von 4Byte einfügen.
-Ein Float mit <b>4Byte</b> ist gut dafür gut geeignet.
-Im Shader-Code, muss dies bei den Uniform-Blöcken nicht beachtet werden.
-
-Bei Verwendung von einem <b>TVector4f</b>, braucht es kein Padding, da dieser 16Byte gross ist.
-*)
-//code+
 type
-  TUBOBuffer = record
+  TSSBOBuffer = record
     Material: record
       ambient: TVector3f;      // Umgebungslicht
       pad0: GLfloat;           // padding 4Byte
@@ -85,26 +58,9 @@ type
       Matrix: Tmat4x4;
       end;
   end;
-  //code-
-
-(*
-So was geht leider <b>nicht</b>.
-Diffuse muss in den nächsten 16Byte-Block !
-//code+
-type    // Unbrauchbare Deklaration !
-  TMaterial = record
-    ambient: TVector3f;      // 3Byte
-    diffuse: TVector3f;      // 3Byte
-    specular: TVector3f;     // 3Byte
-    shininess: GLfloat;      // 3Byte
-  end;
-//code-
-
-Generell wird für ein UBO ein Record empfohlen, mann könnte einen UBO-Buffer auch anders anlegen, zB. in eine Float-Array, dies macht aber wenig Sinn.
-*)
 
 var
-  UBOBuffer: TUBOBuffer;
+  SSBOBuffer: TSSBOBuffer;
 
   SphereVertex, SphereNormal: array of Tmat3x3;
   CubeSize: integer;
@@ -112,28 +68,16 @@ var
 type
   TVB = record
     VAO,
-    VBOvert,            // VBO für Vektor.
-    VBONormal: GLuint;  // VBO für Normale.
+    VBO: GLuint;
   end;
 
-(*
-Für einen <b>UBO</b> wird auch ein <b>Zeiger</b> auf den <b>Puffer</b> gebraucht, ähnlich eines Vertex-Puffers.
-Auch wird eine <b>ID</b> gebraucht, so wie es bei einfachen Uniforms der Fall ist.
-*)
-  //code+
 var
-  UBO: GLuint;        // Puffer-Zeiger
-  UBOBuffer_ID: GLint; // ID im Shader
-  //code-
-
+  SSBO: GLuint;
   VBCube: TVB;
+
   FrustumMatrix,
   WorldMatrix,
   ModelMatrix: TMatrix;
-
-  //  Matrix: TMatrix;
-
-  { TForm1 }
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
@@ -143,10 +87,10 @@ begin
   //remove-
   ogc := TContext.Create(Self);
   ogc.OnPaint := @ogcDrawScene;
-  ogc.OnResize := @ogcResize;   // neues Ereigniss
+  ogc.OnResize := @ogcResize;
 
   CreateScene;
-  InitScene;
+  InitOpenGLDebug;
 end;
 
 procedure TForm1.CalcSphere;
@@ -164,7 +108,7 @@ procedure TForm1.CalcSphere;
   end;
 
 const
-  Sektoren = 32;
+  Sektoren = 36;
 var
   i, j: integer;
   t, rk: single;
@@ -200,14 +144,8 @@ begin
   SetLength(Tab, 0, 0);
 end;
 
-(*
-ID und Puffer generieren.
-Anstelle von <b>glUniformLocation(...</b>, muss man die ID mit <b>glUniformBlockIndex(...</b> auslesen.
-*)
-//code+
 procedure TForm1.CreateScene;
 begin
-  //remove+
   CalcSphere;
 
   CubeSize := 4;
@@ -228,39 +166,21 @@ begin
   Shader.LinkProgramm;
   Shader.UseProgram;
 
-  glGenVertexArrays(1, @VBCube.VAO);
-
-  glGenBuffers(1, @VBCube.VBOvert);
-  glGenBuffers(1, @VBCube.VBONormal);
-
-  glGenBuffers(1, @UBO);                          // UB0-Puffer generieren.
-  //code-
-
-  Timer1.Enabled := True;
-end;
-
-(*
-Material-Daten in den UBO-Puffer laden und binden.
-Pro UBO-Block, wird ein BindingPoint gebraucht.
-Wobei, wen man in mehreren Shader die gleichen Daten laden will, kann man den gleichen BindingPoint verwenden, dazu später.
-*)
-//code+
-procedure TForm1.InitScene;
-begin
   // Material-Werte inizialisieren
-  with UBOBuffer.Material do begin
+  with SSBOBuffer.Material do begin
     ambient := vec3(0.17, 0.01, 0.01);
     diffuse := vec3(0.61, 0.04, 0.04);
     specular := vec3(0.73, 0.63, 0.63);
     shininess := 76.8;
   end;
 
-  // UBO mit Buffer reservieren
-  glBindBuffer(GL_SHADER_STORAGE_BUFFER, UBO);
-  glBufferData(GL_SHADER_STORAGE_BUFFER, SizeOf(TUBOBuffer), nil, GL_DYNAMIC_DRAW);
+  // SSBO mit Buffer reservieren
+  glGenBuffers(1, @SSBO);
+  glBindBuffer(GL_SHADER_STORAGE_BUFFER, SSBO);
+  glBufferData(GL_SHADER_STORAGE_BUFFER, SizeOf(SSBOBuffer), nil, GL_DYNAMIC_DRAW);
 
-  // UBO mit dem Shader verbinden
-  glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 3, UBO);
+  // SSBO mit dem Shader verbinden
+  glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 3, SSBO);
 
   glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
   //code-
@@ -268,20 +188,26 @@ begin
   glClearColor(0.15, 0.15, 0.1, 1.0);
 
   // --- Vertex-Daten für Kugel
+
+  glCreateBuffers(1, @VBCube.VBO);
+  glNamedBufferData(VBCube.VBO, Length(SphereVertex) * SizeOf(Tmat3x3) * 2, nil, GL_STATIC_DRAW);
+  glNamedBufferSubData(VBCube.VBO, 0, Length(SphereVertex) * SizeOf(Tmat3x3), PGLvoid(SphereVertex));
+  glNamedBufferSubData(VBCube.VBO, Length(SphereVertex) * SizeOf(Tmat3x3), Length(SphereNormal) * SizeOf(Tmat3x3), PGLvoid(SphereNormal));
+
+  glGenVertexArrays(1, @VBCube.VAO);
   glBindVertexArray(VBCube.VAO);
 
-  // Vektor
-  glBindBuffer(GL_ARRAY_BUFFER, VBCube.VBOvert);
-  glBufferData(GL_ARRAY_BUFFER, Length(SphereVertex) * SizeOf(Tmat3x3), PGLvoid(SphereVertex), GL_STATIC_DRAW);
+  glVertexAttribBinding(0, 10);
+  glVertexAttribFormat(0, 3, GL_FLOAT, GL_FALSE, 0);
   glEnableVertexAttribArray(0);
-  glVertexAttribPointer(0, 3, GL_FLOAT, False, 0, nil);
 
-  // Normale
-  glBindBuffer(GL_ARRAY_BUFFER, VBCube.VBONormal);
-  glBufferData(GL_ARRAY_BUFFER, Length(SphereNormal) * SizeOf(Tmat3x3), PGLvoid(SphereNormal), GL_STATIC_DRAW);
+  glVertexAttribBinding(1, 10);
+  glVertexAttribFormat(1, 3, GL_FLOAT, GL_FALSE, Length(SphereVertex) * SizeOf(Tmat3x3));
   glEnableVertexAttribArray(1);
-  glVertexAttribPointer(1, 3, GL_FLOAT, False, 0, nil);
 
+  glBindVertexBuffer(10, VBCube.VBO, 0, 12);
+
+  Timer1.Enabled := True;
 end;
 
 procedure TForm1.ogcDrawScene(Sender: TObject);
@@ -298,7 +224,7 @@ begin
 
   glBindVertexArray(VBCube.VAO);
 
-  // --- Zeichne Kugeln
+  // --- Konstruiere Kugeln
 
   d := (7 / (CubeSize * 2 + 1)) * 8;
 
@@ -311,15 +237,15 @@ begin
   for x := -CubeSize to CubeSize do begin
     for y := -CubeSize to CubeSize do begin
       for z := -CubeSize to CubeSize do begin
-        UBOBuffer.Matrix.ModelMatrix.Identity;
-        UBOBuffer.Matrix.ModelMatrix.Translate(x * d, y * d, z * d);                   // Matrix verschieben.
-        UBOBuffer.Matrix.ModelMatrix.Scale(scal);
-        UBOBuffer.Matrix.ModelMatrix := ModelMatrix * UBOBuffer.Matrix.ModelMatrix;
+        SSBOBuffer.Matrix.ModelMatrix.Identity;
+        SSBOBuffer.Matrix.ModelMatrix.Translate(x * d, y * d, z * d);
+        SSBOBuffer.Matrix.ModelMatrix.Scale(scal);
+        SSBOBuffer.Matrix.ModelMatrix := ModelMatrix * SSBOBuffer.Matrix.ModelMatrix;
 
-        UBOBuffer.Matrix.Matrix := FrustumMatrix * WorldMatrix * UBOBuffer.Matrix.ModelMatrix;         // Matrizen multiplizieren.
+        SSBOBuffer.Matrix.Matrix := FrustumMatrix * WorldMatrix * SSBOBuffer.Matrix.ModelMatrix;
 
-        glBindBuffer(GL_SHADER_STORAGE_BUFFER, UBO);
-        glBufferSubData(GL_SHADER_STORAGE_BUFFER, 0, SizeOf(TUBOBuffer), @UBOBuffer);
+        glBindBuffer(GL_SHADER_STORAGE_BUFFER, SSBO);
+        glBufferSubData(GL_SHADER_STORAGE_BUFFER, 0, SizeOf(SSBOBuffer), @SSBOBuffer);
         glDrawArrays(GL_TRIANGLES, 0, Length(SphereVertex) * 3);
       end;
     end;
@@ -333,19 +259,13 @@ begin
   FrustumMatrix.Perspective(45, ClientWidth / ClientHeight, 2.5, 1000.0);
 end;
 
-(*
-Ein UBO muss am Ende wie andere Puffer auch frei gegeben werden.
-*)
-//code+
 procedure TForm1.FormDestroy(Sender: TObject);
 begin
   Shader.Free;
 
   glDeleteVertexArrays(1, @VBCube.VAO);
-  glDeleteBuffers(1, @VBCube.VBOvert);
-  glDeleteBuffers(1, @VBCube.VBONormal);
-  glDeleteBuffers(1, @UBO);  // UBO löschen.
-  //code-
+  glDeleteBuffers(1, @VBCube.VBO);
+  glDeleteBuffers(1, @SSBO);
 end;
 
 procedure TForm1.MenuItemClick(Sender: TObject);
@@ -374,9 +294,6 @@ end;
 //lineal
 
 (*
-Im Shader sind die Material-Daten zu einem Block zusammengefasst, ähnlich einem <b>struct</b> un <b>C++</b>.
-Im Shader wird kein Padding gebraucht.
-
 <b>Vertex-Shader:</b>
 *)
 //includeglsl Vertexshader.glsl
