94f1ce5711e3db2a99545b53406ffcb28baac918
sechshelme <you@example.com>
2023-12-07 19:42:16 +0100

    joint

--- Versuche/84_-_Joints_Animation/05_-_Joints_Versuch/unit1.pas
+++ Versuche/84_-_Joints_Animation/05_-_Joints_Versuch/unit1.pas
@@ -25,6 +25,7 @@ type
     ogc: TContext;
     Shader: TShader;
     procedure CreateScene;
+    procedure CreateJoints;
     procedure ogcDrawScene(Sender: TObject);
   public
   end;
@@ -47,15 +48,18 @@ type
 
 var
   UBOBuffer: TUBOBuffer;
-  QuadVertex: TVectors2f = nil;
-  QuadColor: TVectors3f = nil;
+  QuadVertex: TVectors2f;
+  QuadColor: TVectors3f;
 
 var
   VAO, VBOVektor, VBOColor, UBO: GLint;
   TimerCounter: integer = 0;
 
 const
-  JointCount = 16;
+  JointCount = 8;
+
+var
+  JointsPos: array of single;
 
 
 procedure TForm1.FormCreate(Sender: TObject);
@@ -65,6 +69,7 @@ begin
   //  Height := 480;
   Height := 640;
   //remove-
+  Randomize;
   Timer1.Enabled := False;
   Timer1.Interval := 20;
   Randomize;
@@ -76,13 +81,58 @@ end;
 
 // https://www.youtube.com/watch?app=desktop&v=lDaQ3a43x8A
 
-procedure TForm1.CreateScene;
+procedure TForm1.CreateJoints;
 const
   colors: array of PVector3f = (@vec3blue, @vec3green, @vec3cyan, @vec3red, @vec3magenta, @vec3yellow);
 var
   tmpQuad: TVectors2f = nil;
-  UBO_ID: GLuint;
   i: integer;
+  l: single;
+begin
+  QuadVertex := nil;
+  QuadColor := nil;
+
+  JointsPos := nil;
+  //  JointsPos += [0.0];
+
+  l := 8;
+
+//    JointsPos:=[0,8,16,24,32,40,48,56];
+
+  for i := 0 to JointCount - 1 do begin
+    //if i = 0 then begin
+    //  l := 8;
+    //end else begin
+    //  l := (0.5 + Random) * 8;
+    ////  l := 8;
+    //end;
+    l := 8;
+
+    tmpQuad := nil;
+    tmpQuad.addrectangle;
+    tmpQuad.Scale([l, 4]);
+    tmpQuad.Translate([-l * i, 0.0]);
+    QuadVertex.Add(tmpQuad);
+
+//    JointsPos +=[i* 8];
+
+    if i = 0 then  begin
+      JointsPos +=[ 0];
+    end else begin
+      JointsPos += [(JointsPos[i - 1]) + l];
+    end;
+    //
+
+    WriteLn(JointsPos[i]: 10: 5);
+
+
+    QuadColor.AddRectangleColor(colors[i mod Length(colors)]^);
+  end;
+end;
+
+procedure TForm1.CreateScene;
+var
+  UBO_ID: GLuint;
 begin
   Shader := TShader.Create;
   Shader.LoadShaderObjectFromFile(GL_VERTEX_SHADER, 'Vertexshader.glsl');
@@ -109,15 +159,7 @@ begin
   glClearColor(0.6, 0.6, 0.4, 1.0); // Hintergrundfarbe
   glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-  for i := 0 to JointCount - 1 do begin
-    tmpQuad := nil;
-    tmpQuad.addrectangle;
-    tmpQuad.Scale([8, 4]);
-    tmpQuad.Translate([-8.0 * i, 0.0]);
-    QuadVertex.Add(tmpQuad);
-
-    QuadColor.AddRectangleColor(colors[i mod Length(colors)]^);
-  end;
+  CreateJoints;
 
   // Daten für Quadrat
   glGenVertexArrays(1, @VAO);
@@ -157,9 +199,18 @@ begin
   for i := 0 to JointCount - 1 do begin
     r := sin(TimerCounter * i / 200) / 2;
     Hand.Identity;
-    Hand.TranslateLocalspaceX(-i * 8 + 4);
+    Hand.TranslateLocalspaceX(-JointsPos[i] + 4);
     Hand.RotateC(r);
-    Hand.TranslateLocalspaceX(i * 8 - 4);
+    Hand.TranslateLocalspaceX(JointsPos[i] - 4);
+    //Hand.TranslateLocalspaceX(-i * 8 + 4);
+    //Hand.RotateC(r);
+    //Hand.TranslateLocalspaceX(i * 8 - 4);
+
+//    WriteLn(JointsPos[i]: 10: 5);
+//    WriteLn('--- ', single(i * 8 + 4): 10: 5);
+
+
+
     UBOBuffer.modelMatrix := UBOBuffer.modelMatrix * Hand;
     glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(TUBOBuffer), @UBOBuffer);
     glDrawArrays(GL_TRIANGLES, i * 6, 6);
