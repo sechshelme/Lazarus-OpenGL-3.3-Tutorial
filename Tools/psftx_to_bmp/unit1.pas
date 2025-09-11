2537fad55c4e97151f3a66b0db9de0b9c1779602
sechshelme <m_burkhard@gmx.ch>
2025-09-11 19:09:09 +0200

    png

--- Tools/psftx_to_bmp/unit1.pas
+++ Tools/psftx_to_bmp/unit1.pas
@@ -12,9 +12,7 @@ type
   { TForm1 }
 
   TForm1 = class(TForm)
-    BMP: TButton;
     PNG: TButton;
-    procedure BMPClick(Sender: TObject);
     procedure PNGClick(Sender: TObject);
   private
 
@@ -36,43 +34,7 @@ implementation
 // https://www.zap.org.au/projects/console-fonts-distributed/psftx-opensuse-15.5/index.html
 
 const
-  SourcePath='t.fnt.psftx';
-
-procedure TForm1.BMPClick(Sender: TObject);
-const
-  ofs = 10;
-  size = 22;
-var
-  sl: TStringList;
-  i, y, x: integer;
-  s: string;
-  bit: TBitmap;
-begin
-  bit := TBitmap.Create;
-  bit.SetSize(8 * 256, 16);
-  sl := TStringList.Create;
-  sl.LoadFromFile(SourcePath);
-  for i := 0 to 255 do begin
-    for y := 0 to 15 do begin
-      s := sl[ofs + i * size + y];
-      for x := 0 to 7 do begin
-        if s[x + 2] = '.' then begin
-          Canvas.Pixels[i * 8 + x, y] := clWhite;
-          Write(' ');
-        end else begin
-          Write('X');
-          Canvas.Pixels[i * 8 + x, y] := clBlack;
-          Bit.Canvas.Pixels[i * 8 + x, y] := $FFFFFF;
-        end;
-      end;
-      WriteLn();
-    end;
-    WriteLn(#10);
-  end;
-  sl.Free;
-  bit.SaveToFile('t.fnt.bmp');
-  bit.Free;
-end;
+  SourcePath = 't.fnt.psftx';
 
 procedure TForm1.PNGClick(Sender: TObject);
 const
@@ -82,10 +44,12 @@ var
   sl: TStringList;
   i, y, x: integer;
   s: string;
-  pic:TPicture;
+  pic1, pic2: TPicture;
 begin
-  pic := TPicture.Create;
-  pic.Bitmap.SetSize(8 * 256, 16);
+  pic1 := TPicture.Create;
+  pic2 := TPicture.Create;
+  pic1.Bitmap.SetSize(8 * 256, 16);
+  pic2.Bitmap.SetSize(9 * 256, 16);
   sl := TStringList.Create;
   sl.LoadFromFile(SourcePath);
   for i := 0 to 255 do begin
@@ -98,7 +62,11 @@ begin
         end else begin
           Write('X');
           Canvas.Pixels[i * 8 + x, y] := clBlack;
-         pic.Bitmap.Canvas.Pixels[i * 8 + x, y] := $FFFFFF;
+          pic1.Bitmap.Canvas.Pixels[i * 8 + x, y] := $FFFFFF;
+          pic2.Bitmap.Canvas.Pixels[i * 9 + x, y] := $FFFFFF;
+          if (x = 7) and (i >= 166) and (i <= 223) then  begin
+            pic2.Bitmap.Canvas.Pixels[i * 9 + x + 1, y] := $FFFFFF;
+          end;
         end;
       end;
       WriteLn();
@@ -106,8 +74,10 @@ begin
     WriteLn(#10);
   end;
   sl.Free;
-  pic.SaveToFile('t.fnt.png');
-  pic.Free;
+  pic1.SaveToFile('t.fnt_8x16.png');
+  pic2.SaveToFile('t.fnt_9x16.png');
+  pic1.Free;
+  pic2.Free;
 end;
 
 end.
