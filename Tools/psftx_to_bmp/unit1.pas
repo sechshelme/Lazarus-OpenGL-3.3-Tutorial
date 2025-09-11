91e1759526b2ab6da08549b534c41da0a91a9c06
sechshelme <m_burkhard@gmx.ch>
2025-09-11 17:58:07 +0200

    png

--- Tools/psftx_to_bmp/unit1.pas
+++ Tools/psftx_to_bmp/unit1.pas
@@ -12,8 +12,10 @@ type
   { TForm1 }
 
   TForm1 = class(TForm)
-    Button1: TButton;
-    procedure Button1Click(Sender: TObject);
+    BMP: TButton;
+    PNG: TButton;
+    procedure BMPClick(Sender: TObject);
+    procedure PNGClick(Sender: TObject);
   private
 
   public
@@ -31,7 +33,12 @@ implementation
 
 {$I-}
 
-procedure TForm1.Button1Click(Sender: TObject);
+// https://www.zap.org.au/projects/console-fonts-distributed/psftx-opensuse-15.5/index.html
+
+const
+  SourcePath='t.fnt.psftx';
+
+procedure TForm1.BMPClick(Sender: TObject);
 const
   ofs = 10;
   size = 22;
@@ -44,7 +51,7 @@ begin
   bit := TBitmap.Create;
   bit.SetSize(8 * 256, 16);
   sl := TStringList.Create;
-  sl.LoadFromFile('t.fnt.psftx');
+  sl.LoadFromFile(SourcePath);
   for i := 0 to 255 do begin
     for y := 0 to 15 do begin
       s := sl[ofs + i * size + y];
@@ -65,6 +72,42 @@ begin
   sl.Free;
   bit.SaveToFile('t.fnt.bmp');
   bit.Free;
+end;
+
+procedure TForm1.PNGClick(Sender: TObject);
+const
+  ofs = 10;
+  size = 22;
+var
+  sl: TStringList;
+  i, y, x: integer;
+  s: string;
+  pic:TPicture;
+begin
+  pic := TPicture.Create;
+  pic.Bitmap.SetSize(8 * 256, 16);
+  sl := TStringList.Create;
+  sl.LoadFromFile(SourcePath);
+  for i := 0 to 255 do begin
+    for y := 0 to 15 do begin
+      s := sl[ofs + i * size + y];
+      for x := 0 to 7 do begin
+        if s[x + 2] = '.' then begin
+          Canvas.Pixels[i * 8 + x, y] := clWhite;
+          Write(' ');
+        end else begin
+          Write('X');
+          Canvas.Pixels[i * 8 + x, y] := clBlack;
+         pic.Bitmap.Canvas.Pixels[i * 8 + x, y] := $FFFFFF;
+        end;
+      end;
+      WriteLn();
+    end;
+    WriteLn(#10);
+  end;
+  sl.Free;
+  pic.SaveToFile('t.fnt.png');
+  pic.Free;
 end;
 
 end.
