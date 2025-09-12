c988a0902005c875ada30d202069f3869fff9381
sechshelme <m_burkhard@gmx.ch>
2025-09-12 20:02:47 +0200

    font

--- Tools/psftx_to_bmp/unit1.pas
+++ Tools/psftx_to_bmp/unit1.pas
@@ -5,7 +5,7 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, FileUtil;
 
 type
 
@@ -15,9 +15,9 @@ type
     PNG: TButton;
     procedure PNGClick(Sender: TObject);
   private
-
-  public
-
+    procedure ClearSL(sl: TStringList);
+    function GetSize(sl: TStringList; cols, rows: PInteger): boolean;
+    procedure Convert(const datei: string);
   end;
 
 var
@@ -33,7 +33,11 @@ implementation
 
 // https://www.zap.org.au/projects/console-fonts-distributed/psftx-opensuse-15.5/index.html
 
-procedure ClearSL(sl: TStringList);
+const
+  srcPath = '/home/tux/Schreibtisch/fonts/psftx';
+  desPath = '/home/tux/Schreibtisch/fonts/png';
+
+procedure TForm1.ClearSL(sl: TStringList);
 var
   i: integer;
 begin
@@ -54,64 +58,65 @@ begin
   end;
 end;
 
-function GetRowSL(sl: TStringList): integer;
+function TForm1.GetSize(sl: TStringList; cols, rows: PInteger): boolean;
 var
-  s: string;
+  srow, scol: string;
   e: integer;
+  sa: TAnsiStringArray;
 begin
+  rows^ := 0;
+  cols^ := 0;
+  Result := False;
   if pos('# Size: ', sl[1]) = 1 then begin
-    s := copy(sl[1], 14, 3);
-    val(s, Result, e);
+    Result := True;
+    sa := sl[1].Split(' ');
+    scol := sa[4];
+    srow := sa[6];
+    val(srow, rows^, e);
     if e <> 0 then begin
-      Result := 0;
+      Result := False;
     end;
-    WriteLn(s);
-  end else begin
-    Result := 0;
+    val(scol, cols^, e);
+    if e <> 0 then begin
+      Result := False;
+    end;
   end;
 end;
 
-procedure Convert(const path: string);
+procedure TForm1.Convert(const datei: string);
 var
   sl: TStringList;
-  i, y, x, row: integer;
+  i, y, x, row, col: integer;
   s: string;
   pic8, pic9: TPicture;
-
 begin
+  WriteLn('Datei: ', datei);
   sl := TStringList.Create;
-  sl.LoadFromFile(Path + '.psftx');
-  row := GetRowSL(sl);
-  if row > 0 then begin
-    WriteLn(row);
+  sl.LoadFromFile(srcPath + '/' + datei + '.psftx');
+  if GetSize(sl, @col, @row) then begin
+    WriteLn('size: ', col, 'x', row);
     ClearSL(sl);
-    //  sl.SaveToFile('test.txt');
+    sl.SaveToFile('test.txt');
     pic8 := TPicture.Create;
     pic9 := TPicture.Create;
-    pic8.Bitmap.SetSize(8 * 256, 16);
-    pic9.Bitmap.SetSize(9 * 256, 16);
+    pic8.Bitmap.SetSize((col + 0) * 256, row);
+    pic9.Bitmap.SetSize((col + 1) * 256, row);
     for i := 0 to 255 do begin
-      WriteLn('Glyph: ', i);
-      for y := 0 to 15 do begin
+      for y := 0 to row - 1 do begin
         s := sl[i * row + y];
-        for x := 0 to 7 do begin
-          if s[x + 2] = '.' then begin
-            Write(' ');
-          end else begin
-            Write('X');
-            pic8.Bitmap.Canvas.Pixels[i * 8 + x, y] := $FFFFFF;
-            pic9.Bitmap.Canvas.Pixels[i * 9 + x, y] := $FFFFFF;
-            if (x = 7) and (i >= 166) and (i <= 223) then  begin
-              pic9.Bitmap.Canvas.Pixels[i * 9 + x + 1, y] := $FFFFFF;
+        for x := 0 to col - 1 do begin
+          if s[x + 2] <> '.' then begin
+            pic8.Bitmap.Canvas.Pixels[i * (col + 0) + x, y] := $FFFFFF;
+            pic9.Bitmap.Canvas.Pixels[i * (col + 1) + x, y] := $FFFFFF;
+            if (x = (col - 1)) and (i >= 166) and (i <= 223) then  begin
+              pic9.Bitmap.Canvas.Pixels[i * (col + 1) + x + 1, y] := $FFFFFF;
             end;
           end;
         end;
-        WriteLn();
       end;
-      WriteLn(#10);
     end;
-    pic8.SaveToFile(Path + '_8x16.png');
-    pic9.SaveToFile(Path + '_9x16.png');
+    pic8.SaveToFile(desPath + '/' + datei + '_8x16.png');
+    pic9.SaveToFile(desPath + '/' + datei + '_9x16.png');
     pic8.Free;
     pic9.Free;
   end;
@@ -119,9 +124,22 @@ begin
 end;
 
 procedure TForm1.PNGClick(Sender: TObject);
+var
+  slFiles: TStringList;
+  i: integer;
+  path: string;
 begin
-  Convert('t.fnt');
-  Convert('cp865-8x16.psfu');
+  slFiles := FindAllFiles(srcPath, '*.psftx', False);
+
+  for i := 0 to slFiles.Count - 1 do begin
+    path := ExtractFileName(slFiles[i]);
+    Delete(path, Length(path) - 5, 60);
+    Convert(path);
+    Application.ProcessMessages;
+  end;
+  slFiles.Free;
+
+  WriteLn('Ende');
 end;
 
 end.
