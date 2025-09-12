4aad3e4d587dce4a52cdce1670d2440fad803c20
sechshelme <m_burkhard@gmx.ch>
2025-09-12 15:54:25 +0200

    ffont

--- Tools/psftx_to_bmp/unit1.pas
+++ Tools/psftx_to_bmp/unit1.pas
@@ -33,51 +33,95 @@ implementation
 
 // https://www.zap.org.au/projects/console-fonts-distributed/psftx-opensuse-15.5/index.html
 
-const
-  SourcePath = 't.fnt';
+procedure ClearSL(sl: TStringList);
+var
+  i: integer;
+begin
+  for i := sl.Count - 1 downto 0 do begin
+    if pos('    U+', sl[i]) = 1 then begin
+      sl.Delete(i);
+    end else if pos('GLYPH', sl[i]) = 1 then  begin
+      sl.Delete(i);
+    end else if pos('ENDGLYPH', sl[i]) = 1 then  begin
+      sl.Delete(i);
+    end else if pos('PSFTX FONT', sl[i]) = 1 then  begin
+      sl.Delete(i);
+    end else if pos('#', sl[i]) = 1 then  begin
+      sl.Delete(i);
+    end else if sl[i] = '' then  begin
+      sl.Delete(i);
+    end;
+  end;
+end;
 
-procedure TForm1.PNGClick(Sender: TObject);
-const
-  ofs = 10;
-  size = 22;
+function GetRowSL(sl: TStringList): integer;
+var
+  s: string;
+  e: integer;
+begin
+  if pos('# Size: ', sl[1]) = 1 then begin
+    s := copy(sl[1], 14, 3);
+    val(s, Result, e);
+    if e <> 0 then begin
+      Result := 0;
+    end;
+    WriteLn(s);
+  end else begin
+    Result := 0;
+  end;
+end;
+
+procedure Convert(const path: string);
 var
   sl: TStringList;
-  i, y, x: integer;
+  i, y, x, row: integer;
   s: string;
-  pic1, pic2: TPicture;
+  pic8, pic9: TPicture;
+
 begin
-  pic1 := TPicture.Create;
-  pic2 := TPicture.Create;
-  pic1.Bitmap.SetSize(8 * 256, 16);
-  pic2.Bitmap.SetSize(9 * 256, 16);
   sl := TStringList.Create;
-  sl.LoadFromFile(SourcePath+'.psftx');
-  for i := 0 to 255 do begin
-    for y := 0 to 15 do begin
-      s := sl[ofs + i * size + y];
-      for x := 0 to 7 do begin
-        if s[x + 2] = '.' then begin
-          Canvas.Pixels[i * 8 + x, y] := clWhite;
-          Write(' ');
-        end else begin
-          Write('X');
-          Canvas.Pixels[i * 8 + x, y] := clBlack;
-          pic1.Bitmap.Canvas.Pixels[i * 8 + x, y] := $FFFFFF;
-          pic2.Bitmap.Canvas.Pixels[i * 9 + x, y] := $FFFFFF;
-          if (x = 7) and (i >= 166) and (i <= 223) then  begin
-            pic2.Bitmap.Canvas.Pixels[i * 9 + x + 1, y] := $FFFFFF;
+  sl.LoadFromFile(Path + '.psftx');
+  row := GetRowSL(sl);
+  if row > 0 then begin
+    WriteLn(row);
+    ClearSL(sl);
+    //  sl.SaveToFile('test.txt');
+    pic8 := TPicture.Create;
+    pic9 := TPicture.Create;
+    pic8.Bitmap.SetSize(8 * 256, 16);
+    pic9.Bitmap.SetSize(9 * 256, 16);
+    for i := 0 to 255 do begin
+      WriteLn('Glyph: ', i);
+      for y := 0 to 15 do begin
+        s := sl[i * row + y];
+        for x := 0 to 7 do begin
+          if s[x + 2] = '.' then begin
+            Write(' ');
+          end else begin
+            Write('X');
+            pic8.Bitmap.Canvas.Pixels[i * 8 + x, y] := $FFFFFF;
+            pic9.Bitmap.Canvas.Pixels[i * 9 + x, y] := $FFFFFF;
+            if (x = 7) and (i >= 166) and (i <= 223) then  begin
+              pic9.Bitmap.Canvas.Pixels[i * 9 + x + 1, y] := $FFFFFF;
+            end;
           end;
         end;
+        WriteLn();
       end;
-      WriteLn();
+      WriteLn(#10);
     end;
-    WriteLn(#10);
+    pic8.SaveToFile(Path + '_8x16.png');
+    pic9.SaveToFile(Path + '_9x16.png');
+    pic8.Free;
+    pic9.Free;
   end;
   sl.Free;
-  pic1.SaveToFile(SourcePath+'_8x16.png');
-  pic2.SaveToFile(SourcePath+'_9x16.png');
-  pic1.Free;
-  pic2.Free;
+end;
+
+procedure TForm1.PNGClick(Sender: TObject);
+begin
+  Convert('t.fnt');
+  Convert('cp865-8x16.psfu');
 end;
 
 end.
